b72dcc9abd4da782350e6a96bbf8b77ffaac1ac7
bajacqueli <bastien.jacquelin@etu.uca.fr>
2024-08-17 19:45:32 +0200

    Add profil screen

---
+++ src/screens/Profil.tsx
@@ -0,0 +1,23 @@
+import React, {useState, useRef} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+const Profil = () => {
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <Text>Profil page</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    backgroundColor: 'lightgrey',
+    flex: 1,
+  },
+});
+
+export default Profil;
